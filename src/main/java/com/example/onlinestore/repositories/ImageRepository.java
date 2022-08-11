aaf464cdfc99b00e0d96ce2db325fc3a1d4cd8be
rusvlp <qdrwlad@gmail.com>
2022-08-12 01:36:22 +0300

    Added product editing ability

--- src/main/java/com/example/onlinestore/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.onlinestore.repositories;
-
-import com.example.onlinestore.entites.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ImageRepository extends JpaRepository<Long, Image> {
-}
